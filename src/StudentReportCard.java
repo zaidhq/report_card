e010f56b23140713a528a0bf6cd8f5c16a201eac
zaidhq <zaidhq@gmail.com>
2019-07-29 17:57:37 +0530

    Used hashmap in complete code replaced arrays and arralist

--- src/StudentReportCard.java
+++ src/StudentReportCard.java
@@ -1,46 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class StudentReportCard {
     public static void main(String[] args) {
-        int numberOfStudents = 5;
-        String[] student = new String[numberOfStudents];
-        int[] marksInEnglish = new int[numberOfStudents];
-        int[] marksInHindi = new int[numberOfStudents];
-        int[] marksInMaths = new int[numberOfStudents];
+        int numberOfStudents = 1;
+        HashMap<String, Object> studentInfo = new HashMap<String, Object>();
+
         StudentReportCard studentReportCard = new StudentReportCard();
 
         for (int i = 0; i < numberOfStudents; i++) {
+
             Scanner scanner = new Scanner(System.in);  // Create a Scanner object
             System.out.println("Enter student name");
-            student[i] = scanner.nextLine();  // Read user input  //nextLine vs next
-
+            studentInfo.put("name", scanner.nextLine());
 
             System.out.println("Marks in English ?");
-            marksInEnglish[i] = scanner.nextInt();
+            studentInfo.put("marksInEnglish", scanner.nextInt());
             System.out.println("Marks in Hindi ?");
-            marksInHindi[i] = scanner.nextInt();
+            studentInfo.put("marksInHindi", scanner.nextInt());
             System.out.println("Marks in maths ?");
-            marksInMaths[i] = scanner.nextInt();
+            studentInfo.put("marksInMaths", scanner.nextInt());
 
 
         }
         for (int counter = 0; counter < numberOfStudents; counter++) {
 
-            int total = marksInEnglish[counter] + marksInHindi[counter] + marksInMaths[counter];
+            int total = Integer.parseInt(studentInfo.get("marksInEnglish").toString()) + Integer.parseInt(studentInfo.get("marksInHindi").toString()) + Integer.parseInt(studentInfo.get("marksInMaths").toString());
             int percentage = (total * 100) / 300;
-            System.out.println(student[counter]);
+            System.out.println(studentInfo.get("name"));
             System.out.println("Subject    Marks   Grades");
-            System.out.println("English    " + marksInEnglish[counter] + "      " + studentReportCard.getGrade(marksInEnglish[counter]));
-            System.out.println("Hindi      " + marksInHindi[counter] + "      " + studentReportCard.getGrade(marksInEnglish[counter]));
-            System.out.println("Maths      " + marksInMaths[counter] + "      " + studentReportCard.getGrade(marksInMaths[counter]));
+            System.out.println("English    " + studentInfo.get("marksInEnglish") + "      " + studentReportCard.getGrade(Integer.parseInt(studentInfo.get("marksInEnglish").toString())));
+            System.out.println("Hindi      " + studentInfo.get("marksInHindi") + "      " + studentReportCard.getGrade(Integer.parseInt(studentInfo.get("marksInHindi").toString())));
+            System.out.println("Maths      " + studentInfo.get("marksInMaths") + "      " + studentReportCard.getGrade(Integer.parseInt(studentInfo.get("marksInMaths").toString())));
             System.out.println("Total      " + total + "      " + studentReportCard.getGrade(percentage));
 
 
-            if (marksInEnglish[counter] < 40) {
+            if (Integer.parseInt(studentInfo.get("marksInEnglish").toString()) < 40) {
                 System.out.println("Fail");
-            } else if (marksInHindi[counter] < 40) {
+            } else if (Integer.parseInt(studentInfo.get("marksInHindi").toString()) < 40) {
                 System.out.println("Fail");
-            } else if (marksInMaths[counter] < 40) {
+            } else if (Integer.parseInt(studentInfo.get("marksInMaths").toString()) < 40) {
                 System.out.println("Fail");
             } else {
                 System.out.println("Pass");
@@ -48,7 +48,7 @@ public class StudentReportCard {
         }
     }
 
-    public String getGrade(int marks) {
+    private String getGrade(int marks) {
         if (marks >= 90 && marks <= 100)
             return "A+";
         else if (marks >= 80 && marks < 90)
